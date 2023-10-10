d9a530870df5281130031f38410d572c0445dc0b
Irtiza Ashraf <irtizaashraf802@gmail.com>
2023-10-10 15:53:24 +0500

    process solution

--- login.js
+++ login.js
@@ -1,4 +1,42 @@
 
+// const loginForm = document.getElementById('loginForm');
+
+// loginForm.addEventListener('submit', function(event) {
+//     event.preventDefault();
+
+//     const enteredUsername = document.getElementById('username').value;
+//     const enteredPassword = document.getElementById('password').value;
+
+//     // Retrieve user data from local storage
+//     const storedUser = JSON.parse(localStorage.getItem('user')) || [];
+
+//     const matchedUserdata = storedUser.find(user => user.name === enteredUsername && user.password === enteredPassword); 
+// //   console.log(matchedUserdata);
+//     if (matchedUserdata) {
+//         Swal.fire({
+//             title: 'Custom animation with Animate.css',
+//             showClass: {
+//               popup: 'animate__animated animate__fadeInDown'
+//             },
+//             hideClass: {
+//               popup: 'animate__animated animate__fadeOutUp'
+//             }
+//         })
+//         // Redirect to another page or perform other actions after successful login
+//         window.location.href = 'index.html';
+//     } else {
+//         alert('Login Failed. Please check your username and password.');
+//     }
+   
+// });
+
+
+
+
+// storedUser && enteredUsername === storedUser.username && enteredPassword === storedUser.password
+
+
+
 const loginForm = document.getElementById('loginForm');
 
 loginForm.addEventListener('submit', function(event) {
@@ -10,26 +48,24 @@ loginForm.addEventListener('submit', function(event) {
     // Retrieve user data from local storage
     const storedUser = JSON.parse(localStorage.getItem('user')) || [];
 
-    const matchedUserdata = storedUser.map(user => user.name === enteredUsername && user.password === enteredPassword); 
-//   console.log(matchedUserdata);
+    const matchedUserdata = storedUser.find(user => user.name === enteredUsername && user.password === enteredPassword); 
+
     if (matchedUserdata) {
         Swal.fire({
-            title: 'Custom animation with Animate.css',
-            showClass: {
-              popup: 'animate__animated animate__fadeInDown'
-            },
-            hideClass: {
-              popup: 'animate__animated animate__fadeOutUp'
+            title: 'Login Successful!',
+            text: 'Redirecting to the homepage...',
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 1500,
+            willClose: () => {
+                window.location.href = 'index.html'; // Redirect after the alert is closed
             }
-        })
-        // Redirect to another page or perform other actions after successful login
+        });
     } else {
-        alert('Login Failed. Please check your username and password.');
+        Swal.fire({
+            title: 'Login Failed!',
+            text: 'Please check your username and password.',
+            icon: 'error'
+        });
     }
-    window.location.href = 'index.html';
 });
-
-
-
-
-// storedUser && enteredUsername === storedUser.username && enteredPassword === storedUser.password
